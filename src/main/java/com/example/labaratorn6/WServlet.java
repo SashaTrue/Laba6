0cee27c54ad06f63de3d2858b1bee15385e27546
Александр Трушников <TrushnikovSasha@gmail.com>
2023-04-03 10:26:50 +0600

    HZ

---
+++ src/main/java/com/example/labaratorn6/WServlet.java
@@ -0,0 +1,71 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.example.labaratorn6;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({"/w-servlet"})
+public class WServlet extends HttpServlet {
+    Random rnd = new Random();
+    int answer;
+    private String message;
+
+    public WServlet() {
+        this.answer = this.rnd.nextInt(1000);
+        this.message = primerchik(this.answer, 3);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + this.message + "</h1>");
+        out.println("<a href='WR.jsp'>" + this.answer + "</a>");
+        out.println("<a href='WW.jsp'>" + (this.answer - this.rnd.nextInt(10)) + "</a>");
+        out.println("</body></html>");
+    }
+
+    public static String primerchik(int num, int dif) {
+        Random rnd = new Random(num);
+        int res = rnd.nextInt(num);
+        String temp = Integer.toString(res);
+        int znak = 0;
+
+        for(int i = 0; i < dif; i++) {
+            int k = rnd.nextInt(num);
+            znak = rnd.nextInt(2);
+            switch (znak) {
+                case 0:
+                    temp += "+";
+                    temp += Integer.toString(k);
+                    res += k;
+                    break;
+                case 1:
+                    temp = temp + "-";
+                    temp = temp + Integer.toString(k);
+                    res -= k;
+            }
+        }
+
+        if (res > num) {
+            temp = temp + "-";
+            temp = temp + Integer.toString(res - num);
+            res -= res - num;
+        } else if (res < num) {
+            temp = temp + "+";
+            temp = temp + Integer.toString(num - res);
+            res += num - res;
+        }
+
+        return temp;
+    }
+}
---
+++ src/main/webapp/RW.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Неправильно((      2/3" %>
+</h1>
+<br/>
+<a href="rw-servlet">Далее</a>
+</body>
+</html>
